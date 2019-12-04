b03721e1726fc59db54833a8727ee661b961f583
Jacopo14 <pagliucajacopo@gmail.com>
2019-12-04 19:17:24 +0100

    ritorno provvisorio

--- f_tunnel_2dot.m
+++ f_tunnel_2dot.m
@@ -1,6 +1,6 @@
 % calcolo probabilità di tunneling per transistor con 2 dot
 
-function y = f_tunnel(j,n,Vd,Vg)
+function y = f_tunnel_2dot(j,n,Vd,Vg)
 
 e = 1;
 Cg = 0.8;
@@ -20,6 +20,8 @@ dE_dot2_dot1 = -e*V1+e*V2+(e^2)/2*C;
 dE_dot2_d = -e*Vd+e*V2+(e^2)/2*C;
 dE_d_dot2 = e*Vd-e*V2+(e^2)/2*C;
 
+dE = [dE_s_dot1, dE_dot1_s, dE_dot1_dot2, dE_dot2_dot1, dE_dot2_d, dE_d_dot2];
 
-
+%provvisorio
+y = 0.001*dE(j)/(exp(5*dE(j))-1);
 
