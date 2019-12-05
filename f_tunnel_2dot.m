29d64290e82259402b7999f7bed07230f8d4eb8c
Jacopo14 <pagliucajacopo@gmail.com>
2019-12-05 12:01:09 +0100

    coefficienti

--- f_tunnel_2dot.m
+++ f_tunnel_2dot.m
@@ -25,4 +25,8 @@ dE = [dE_s_dot1, dE_dot1_s, dE_dot1_dot2, dE_dot2_dot1, dE_dot2_d, dE_d_dot2];
 %provvisorio 
 %y = [0.001*dE(1)/(exp(5*dE(1)-1)),0.001*dE(2)/(exp(5*dE(2)-1)),0.001*dE(3)/(exp(5*dE(3)-1)),0.001*dE(4)/(exp(5*dE(4)-1)),0.001*dE(5)/(exp(5*dE(5)-1)),0.001*dE(6)/(exp(5*dE(6)-1))];
 
-y = 0.001*dE(j)/(exp(5*dE(j)-1));
+kb=1.381e-23;   % Boltzman constant (J/K)
+%r=15*1000000;     % tunnel resistance R1 (Ohm)
+r=25;
+
+y = 1/(r*e^2)*dE(j)/(1-exp(dE(j)/kb));
--- montecarlo_2dot.m
+++ montecarlo_2dot.m
@@ -1,13 +1,13 @@
 clear all; close all;
 
 y = [];
-n = 5;
+n = 20;
 N1 = -n:n;
 N2 = -n:n;
 for k = 1:6
     for i = 1:(2*n+1)
         for j = 1:(2*n+1)
-            F(i,j,k)=f_tunnel_2dot(k,i-n,j-n,1,0);
+            F(i,j,k) = f_tunnel_2dot(k,i-n,j-n,1,0);
         end
     end
 end
@@ -19,13 +19,3 @@ hold on; surf(N1,N2,F(:,:,4),'FaceAlpha',0.5);
 hold on; surf(N1,N2,F(:,:,5),'FaceAlpha',0.5);
 hold on; surf(N1,N2,F(:,:,6),'FaceAlpha',0.5);
 
-
-% for i = -n : n
-%     y = [y; f_tunnel_2dot(1,i,5,1,0)];
-% end
-% %%% plot dei 4 tipi di gamma(n) per n che va da -10 a 10
-% figure ('Name','plot dei 4 tipi di gamma(n) per n che va da -10 a 10','NumberTitle','off');
-% plot(N, y(:,1), N, y(:,2), N, y(:,3), N, y(:,4), N, y(:,5), N, y(:,6));
-% xlabel ('Numero di cariche nel dot (n)');
-% ylabel ('Rate di tunneling (\Gamma_n)');
-% legend ('\Gamma_{source->dot1}', '\Gamma_{dot1->source}', '\Gamma_{dot1->dot2}', '\Gamma_{dot2->dot1}', '\Gamma_{dot2->drain}', '\Gamma_{drain->dot2}')
