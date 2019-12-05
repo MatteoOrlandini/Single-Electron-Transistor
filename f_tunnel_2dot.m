83a54ec3fe0202ffb2d0def3c300a4310e30fa6f
Jacopo14 <pagliucajacopo@gmail.com>
2019-12-05 11:13:21 +0100

    3d plot

--- f_tunnel_2dot.m
+++ f_tunnel_2dot.m
@@ -23,5 +23,6 @@ dE_d_dot2 = e*Vd-e*V2+(e^2)/2*C;
 dE = [dE_s_dot1, dE_dot1_s, dE_dot1_dot2, dE_dot2_dot1, dE_dot2_d, dE_d_dot2];
 
 %provvisorio 
-y = [0.001*dE(1)/(exp(5*dE(1)-1)),0.001*dE(2)/(exp(5*dE(2)-1)),0.001*dE(3)/(exp(5*dE(3)-1)),0.001*dE(4)/(exp(5*dE(4)-1)),0.001*dE(5)/(exp(5*dE(5)-1)),0.001*dE(6)/(exp(5*dE(6)-1))];
+%y = [0.001*dE(1)/(exp(5*dE(1)-1)),0.001*dE(2)/(exp(5*dE(2)-1)),0.001*dE(3)/(exp(5*dE(3)-1)),0.001*dE(4)/(exp(5*dE(4)-1)),0.001*dE(5)/(exp(5*dE(5)-1)),0.001*dE(6)/(exp(5*dE(6)-1))];
 
+y = 0.001*dE(j)/(exp(5*dE(j)-1));
--- montecarlo_2dot.m
+++ montecarlo_2dot.m
@@ -1,15 +1,31 @@
 clear all; close all;
 
 y = [];
-n = 10;
-N = -n:n;
-
-for i = -n : n
-    y = [y; f_tunnel_2dot(1,i,5,1,0)];
+n = 5;
+N1 = -n:n;
+N2 = -n:n;
+for k = 1:6
+    for i = 1:(2*n+1)
+        for j = 1:(2*n+1)
+            F(i,j,k)=f_tunnel_2dot(k,i-n,j-n,1,0);
+        end
+    end
 end
-%%% plot dei 4 tipi di gamma(n) per n che va da -10 a 10
-figure ('Name','plot dei 4 tipi di gamma(n) per n che va da -10 a 10','NumberTitle','off');
-plot(N, y(:,1), N, y(:,2), N, y(:,3), N, y(:,4), N, y(:,5), N, y(:,6));
-xlabel ('Numero di cariche nel dot (n)');
-ylabel ('Rate di tunneling (\Gamma_n)');
-legend ('\Gamma_{source->dot1}', '\Gamma_{dot1->source}', '\Gamma_{dot1->dot2}', '\Gamma_{dot2->dot1}', '\Gamma_{dot2->drain}', '\Gamma_{drain->dot2}')
+
+figure; surf(N1,N2,F(:,:,1),'FaceAlpha',0.5);
+hold on; surf(N1,N2,F(:,:,2),'FaceAlpha',0.5);
+hold on; surf(N1,N2,F(:,:,3),'FaceAlpha',0.5);
+hold on; surf(N1,N2,F(:,:,4),'FaceAlpha',0.5);
+hold on; surf(N1,N2,F(:,:,5),'FaceAlpha',0.5);
+hold on; surf(N1,N2,F(:,:,6),'FaceAlpha',0.5);
+
+
+% for i = -n : n
+%     y = [y; f_tunnel_2dot(1,i,5,1,0)];
+% end
+% %%% plot dei 4 tipi di gamma(n) per n che va da -10 a 10
+% figure ('Name','plot dei 4 tipi di gamma(n) per n che va da -10 a 10','NumberTitle','off');
+% plot(N, y(:,1), N, y(:,2), N, y(:,3), N, y(:,4), N, y(:,5), N, y(:,6));
+% xlabel ('Numero di cariche nel dot (n)');
+% ylabel ('Rate di tunneling (\Gamma_n)');
+% legend ('\Gamma_{source->dot1}', '\Gamma_{dot1->source}', '\Gamma_{dot1->dot2}', '\Gamma_{dot2->dot1}', '\Gamma_{dot2->drain}', '\Gamma_{drain->dot2}')
